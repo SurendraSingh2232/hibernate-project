dd9f46cd2dea44574f6b753740338d539c806f94
Surendra Singh <surendra.singh2232@outlook.com>
2022-01-16 10:52:59 +0530

    done

--- src/main/java/com/sunglowsys/test/UpdatePatient.java
+++ src/main/java/com/sunglowsys/test/UpdatePatient.java
@@ -1,4 +1,13 @@
 package com.sunglowsys.test;
 
+import com.sunglowsys.domain.Patient;
+import com.sunglowsys.service.PatientService;
+import com.sunglowsys.service.PatientServiceImpl;
+
 public class UpdatePatient {
+    public static void main(String[] args) {
+        Patient patient = new Patient();
+        patient.setFirstName("patient");
+        new PatientServiceImpl().updatePatient(patient,1L);
+    }
 }
